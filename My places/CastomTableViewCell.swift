20134d4e218bfd25a3082f8132c360c198d034e5
Arthur <Fokusnik909@mail.ru>
2023-05-21 15:50:06 +0500

    custom view cell

---
+++ My places/CastomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CastomTableViewCell.swift
+//  My places
+//
+//  Created by Артур  Арсланов on 21.05.2023.
+//
+
+import UIKit
+
+class CastomTableViewCell: UITableViewCell {
+
+  
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
